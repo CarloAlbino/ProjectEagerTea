4b1482783b696faf6774db984a4b780158a029ee
josh16 <josh.randall16@gmail.com>
2017-06-15 01:20:40 -0400

    Created Range Function
    -Created a Range function
    -This function will be using the 'Lists' Method
    -Made some comments of what will be used and what is needed to set up
    the range

--- Assets/_Scripts/Prototyping/PlayerMovement.cs
+++ Assets/_Scripts/Prototyping/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using AI;
 
+
 public class PlayerMovement : MonoBehaviour {
 
     //Reference to the Grid2 script
@@ -72,20 +73,40 @@ public class PlayerMovement : MonoBehaviour {
         //First click only
         if (startNode == null)
         {
-            //Reference "this"
+            //Reference "this" class
             if (node == m_grid.NodeFromWorldPoint(this.transform.position))
-                {
+            {
 
                 //Set the start node as the position of the player
                 startNode = node;
                 m_grid = startNode.GetComponentInParent<Grid>();
                 node.SetColour(Colors.Red); //Starting node gets set to Red
-                }
-             
+
+
+             }
+
+            else if (endNode == null)
+             {
+                //Range //Lists (return a list of nodes)
+
+
+                endNode = node;
+                path = pathfinding.GetPath(m_grid, startNode, endNode); // Asks for a path here (grid, start and end node are passd in)
+                node.SetColour(Colors.Yellow);
+            }
+            else
+            {
+                startNode.ResetColour();
+                endNode.ResetColour();
+                startNode = node;
+                endNode = null;
+                node.SetColour(Colors.Red);
+            }
+
             //----Work in Progress----
         }
 
-
+        /*
         else if (endNode == null)
         {
             endNode = node;
@@ -100,21 +121,39 @@ public class PlayerMovement : MonoBehaviour {
             endNode = null;
             node.SetColour(Colors.Red);
         }
-
+        */
     }
 
 
 
 
+    //The Range Function (How far the player can move)
+    public void Range()
+    {
+        /*
+         -We will Create a List here and we will use the 'Count'
+         Property to get the number of elements in the list(The nodes).
+        
+        -If the amount of nodes are the Range limit, the player cannot move
+        any further then that.
+         
+        - We will use the 'Capacity' Property to set the Players Range (How far the player can move)
+        */
+
+        //1. Declare the List
+        List<Node> playerRange = new List<Node>();
+
+        //2. Get the Capacity
+              //code
+
+        //3. Set the Capacity
+              //Code
+    }
 
 
+   
 
-
-
-
-
-
-
+    
 
 }
 
